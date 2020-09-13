03a280a65c00b89353c586608878cd9eb4a3785c
snap-interns <michael@snapit.group>
2020-09-13 16:57:56 +1200

    added linear regression and performance metrics

--- part1.py
+++ part1.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
+
 
 dataPath = "data/diamonds.csv"
 cuts = ["fair", "good", "very good", "ideal", "premium"]
 colours = ["E", "I", "J", "H", "G", "D", "F"]
-clarity = ["SI1", "SI2", "VS1", "VS2", "VVS1", "VVS2", "SI1", "I1"]
+clarity = ["SI1", "SI2", "VS1", "VS2", "VVS1", "VVS2", "SI1", "I1", "IF"]
 
 def preprocessData(path):
     data = pd.read_csv(path)
@@ -24,5 +31,27 @@ def preprocessData(path):
     return data
 
 data = preprocessData(dataPath)
-data.to_csv("preprocessedData.csv", index = False)
+# data.to_csv("preprocessedData.csv", index = False)
+
+train, test = train_test_split(data, test_size=0.3, random_state=309, shuffle=True)
+
+y_train = train.iloc[:,-1]
+x_train = train.iloc[:,:-1] 
+
+y_test = test.iloc[:,-1]
+x_test = test.iloc[:,:-1]
+
+rf = LinearRegression()
+rf.fit(x_train, y_train)
+
+
+predictions = rf.predict(x_test)
+
+# test_predictions = rf.predict(test_data)
+
+print("on y_test")
+print("Mean squared error: ", np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+print("R2 score: ", metrics.r2_score(y_test, predictions))
+
+
 print(data.head())
